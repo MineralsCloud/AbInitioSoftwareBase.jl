63e610921e4ae198cbde8b62dcd7ae180f2611b5
singularitti <singularitti@outlook.com>
2020-06-25 05:38:45 -0400

    Move `MpiCmd` from `Express.CLI` to here

--- src/CLI.jl
+++ src/CLI.jl
@@ -1,3 +1,29 @@
 module CLI
 
+export MpiCmd
+
+struct MpiCmd
+    n::UInt
+    bin
+    host
+    arch
+    wdir
+    path
+    file
+    configfile
+    env
+end
+MpiCmd(
+    n;
+    bin = "mpiexec",
+    host = "",
+    arch = "",
+    wdir = "",
+    path = String[],
+    file = "",
+    configfile = "",
+    env = ENV,
+) = MpiCmd(n, bin, host, arch, wdir, path, file, configfile, env)
+# The docs are from https://www.mpich.org/static/docs/v3.3/www1/mpiexec.html.
+
 end
