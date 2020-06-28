87cd762011e705fff1eb8572985fdf5631097d3f
singularitti <singularitti@outlook.com>
2020-06-28 07:39:46 -0400

    Add `write_input` from Express.jl

--- src/Inputs.jl
+++ src/Inputs.jl
@@ -1,11 +1,28 @@
 module Inputs
 
-export Input, inputstring, titleof
+export Input, inputstring, titleof, write_input
 
 abstract type Input end
 
 function inputstring end
 
 function titleof end
+
+function write_input(file, object::Input, dry_run = false)
+    if dry_run
+        if isfile(file)
+            @warn "file `$file` will be overwritten!"
+        else
+            @warn "file `$file` will be created!"
+        end
+        print(inputstring(object))
+    else
+        mkpath(dirname(file))
+        open(file, "w") do io
+            write(io, inputstring(object))
+        end
+    end
+    return
+end
 
 end
