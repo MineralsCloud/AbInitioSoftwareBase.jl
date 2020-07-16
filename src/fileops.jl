2dacf537d3afcab5df8fdaa6137a073ff8b4c8f9
singularitti <singularitti@outlook.com>
2020-07-15 23:46:36 -0400

    Add docs to `save`, `load` & `extension`
    in src/fileops.jl

--- src/fileops.jl
+++ src/fileops.jl
@@ -7,6 +7,18 @@ export FilePath, load, save
 
 const FilePath = AbstractPath
 
+"""
+    save(filepath, data)
+
+Save `data` to `filepath`.
+
+By now, `YAML`, `JSON`, and `TOML` formats are supported. The format is recognized by `filepath` extension.
+
+!!! warning
+    Allowed `data` types can be referenced in [`JSON.jl` documentation](https://github.com/JuliaIO/JSON.jl/blob/master/README.md)
+    and [`YAML.jl` documentation.](https://github.com/JuliaData/YAML.jl/blob/master/README.md)
+    For `TOML` format, only `AbstractDict` type is allowed.
+"""
 function save(filepath, data)
     ext, fp = extension(filepath), abspath(expanduser(filepath))
     if ext ∈ ("yaml", "yml")
@@ -25,6 +37,13 @@ function save(filepath, data)
     end
 end # function save
 
+"""
+    save(filepath, data)
+
+load `data` from `filepath` to a `Dict`.
+
+By now, `YAML`, `JSON`, and `TOML` formats are supported. The format is recognized by `filepath` extension.
+"""
 function load(filepath)
     ext, fp = extension(filepath), abspath(expanduser(filepath))
     if ext ∈ ("yaml", "yml")
@@ -40,6 +59,7 @@ function load(filepath)
     end
 end # function load
 
+"Get the extension from `filepath`. Return an empty string if no extension is found."
 function extension(filepath)  # From https://github.com/rofinn/FilePathsBase.jl/blob/af850a4/src/path.jl#L331-L340
     name = basename(filepath)
     tokenized = split(name, '.')
