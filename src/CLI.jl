3e9975161f73339758cae2a86dccfb941c23e10f
singularitti <singularitti@outlook.com>
2020-07-15 23:58:09 -0400

    Add docs to `MpiCmd`

--- src/CLI.jl
+++ src/CLI.jl
@@ -2,14 +2,29 @@ module CLI
 
 export MpiCmd
 
+"""
+    MpiCmd(n; bin = "mpiexec", host = "", arch = "", wdir = "", path = String[], file = "", configfile = "")
+
+Represent the `mpiexec` command. Must be combined with an actual command.
+
+Type `?MpiCmd.host` to see the documentation of the `host` parameter, and so on.
+"""
 struct MpiCmd
+    "Specify the number of processes to use."
     n::UInt
+    "The path to the executable of `mpiexec`."
     bin
+    "Name of host on which to run processes."
     host
+    "Pick hosts with this architecture type."
     arch
+    "Change directory to this one before running executable."
     wdir
+    "Use this to find the executable."
     path
+    "Implementation-defined specification file."
     file
+    "A file containing specifications of host/program, one per line, with number as a comment indicator, e.g., the usual mpiexec input, but with \":\" replaced with a newline. That is, the `configfile` contains lines with `-soft`, `-n`, etc."
     configfile
 end
 MpiCmd(
