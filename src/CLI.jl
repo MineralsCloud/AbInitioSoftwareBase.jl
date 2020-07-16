7048109f01c99176c9f25dd85d763e02e8b095bf
singularitti <singularitti@outlook.com>
2020-07-16 02:46:42 -0400

    Redefine `MpiExec` to `MpiLauncher`

--- src/CLI.jl
+++ src/CLI.jl
@@ -1,39 +1,27 @@
 module CLI
 
-export MpiExec
+export MpiLauncher
 
 """
-    MpiCmd(n; host = "", arch = "", wdir = "", path = String[], file = "", configfile = "")
+    MpiLauncher(n; bin = "mpiexec", host = [], hostfile = "", args = [])
 
-Represent the `mpiexec` command. Must be combined with an actual command.
+Represent the `mpiexec` or `mpirun` executable. Must be combined with an actual command.
 
 Type `?MpiCmd.host` to see the documentation of the `host` parameter, and so on.
 """
-struct MpiExec
+struct MpiLauncher
     "Specify the number of processes to use."
     n::UInt
-    "Name of host on which to run processes."
-    host
-    "Pick hosts with this architecture type."
-    arch
-    "Change directory to this one before running executable."
-    wdir
-    "Use this to find the executable."
-    path
-    "Implementation-defined specification file."
-    file
-    "A file containing specifications of host/program, one per line, with number as a comment indicator, e.g., the usual mpiexec input, but with \":\" replaced with a newline. That is, the `configfile` contains lines with `-soft`, `-n`, etc."
-    configfile
+    "Path to the `mpiexec` or `mpirun` executable."
+    bin
+    "List of hosts on which to invoke processes."
+    host::Vector
+    "Provide a hostfile to use."
+    hostfile
+    "Other args to be specified. Must be a `Vector` of `Pair`s."
+    args::Vector
 end
-MpiExec(
-    n;
-    host = "",
-    arch = "",
-    wdir = "",
-    path = String[],
-    file = "",
-    configfile = "",
-) = MpiExec(n, host, arch, wdir, path, file, configfile)
-# The docs are from https://www.mpich.org/static/docs/v3.3/www1/mpiexec.html.
+MpiLauncher(n; bin = "mpiexec", host = String[], hostfile = "", args = Pair[]) =
+    MpiLauncher(n, bin, host, hostfile, args)
 
 end
