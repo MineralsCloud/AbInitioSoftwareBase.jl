1f5588cbdbaf9b99897c2fb1abae459ae0cad010
singularitti <singularitti@outlook.com>
2020-07-18 02:48:48 -0400

    Rename field `n` to `np` of `MpiLauncher`

--- src/CLI.jl
+++ src/CLI.jl
@@ -3,15 +3,15 @@ module CLI
 export MpiLauncher
 
 """
-    MpiLauncher(n; bin = "mpiexec", host = [], hostfile = "", args = [])
+    MpiLauncher(np; bin = "mpiexec", host = [], hostfile = "", args = Pair[])
 
 Represent the `mpiexec` or `mpirun` executable. Must be combined with an actual command.
 
-Type `?MpiCmd.host` to see the documentation of the `host` parameter, and so on.
+Type `?MpiLauncher.host` to see the documentation of the `host` parameter, and so on.
 """
 struct MpiLauncher
     "Specify the number of processes to use."
-    n::UInt
+    np::UInt
     "Path to the `mpiexec` or `mpirun` executable."
     bin
     "List of hosts on which to invoke processes."
@@ -21,7 +21,7 @@ struct MpiLauncher
     "Other args to be specified. Must be a `Vector` of `Pair`s."
     args::Vector
 end
-MpiLauncher(n; bin = "mpiexec", host = String[], hostfile = "", args = Pair[]) =
-    MpiLauncher(n, bin, host, hostfile, args)
+MpiLauncher(np; bin = "mpiexec", host = String[], hostfile = "", args = Pair[]) =
+    MpiLauncher(np, bin, host, hostfile, args)
 
 end
