1e5ee532cfd91c1bffbaac5ca78c03216f7875b2
singularitti <singularitti@outlook.com>
2020-07-22 00:29:59 -0400

    Recover `writeinput`

--- src/Inputs.jl
+++ src/Inputs.jl
@@ -16,19 +16,24 @@ function writeinput(io::IO, object::Input)
 end # function writeinput
 """
     writeinput(io::IO, object::Input)
-    writeinput(file, object::Input)
+    writeinput(file, object::Input, dry_run::Bool = false)
 
-Write an `Input` object to `file` or `io`.
+Write an `Input` object to `file` or `io`. Use `dry_run = true` to preview changes.
 """
-function writeinput(file, object::Input)
-    if isfile(file)
-        @warn "file `$file` will be overwritten!"
+function writeinput(file, object::Input, dry_run::Bool = false)
+    if dry_run
+        if isfile(file)
+            @warn "file `$file` will be overwritten!"
+        else
+            @warn "file `$file` will be created!"
+        end
+        println("The following will be write to file `$file`:")
+        print(inputstring(object))
     else
-        @warn "file `$file` will be created!"
-    end
-    mkpath(dirname(file))
-    open(file, "w") do io
-        write(io, inputstring(object))
+        mkpath(dirname(file))
+        open(file, "w") do io
+            write(io, inputstring(object))
+        end
     end
     return
 end  # See https://github.com/JuliaLang/julia/blob/3608c84/stdlib/DelimitedFiles/src/DelimitedFiles.jl#L787-L791
