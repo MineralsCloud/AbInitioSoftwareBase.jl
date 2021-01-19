6d9b8bcaecefdf17a2fc7045ec03911700cb90fd
singularitti <singularitti@outlook.com>
2021-01-19 14:44:06 -0500

    Add back `Mpiexec`

--- src/CLI.jl
+++ src/CLI.jl
@@ -1,8 +1,21 @@
 module CLI
 
-export Executable, scriptify
+export Executable, Mpiexec, scriptify
 
 abstract type Executable end
+"""
+    Mpiexec(np; bin = "mpiexec", host = [], hostfile = "", args = Pair[])
+
+Represent the `mpiexec` or `mpirun` executable. Must be combined with an actual command.
+
+Type `?Mpiexec.host` to see the documentation of the `host` parameter, and so on.
+"""
+struct Mpiexec <: Executable
+    "Specify the number of processes to use."
+    np::UInt
+    "Path to the `mpiexec` or `mpirun` executable."
+    args::Vector{<:Pair}
+end
 
 function scriptify end
 
