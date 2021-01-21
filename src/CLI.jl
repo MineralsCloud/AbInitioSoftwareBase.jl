ee57b979a0f89ab596aba7d8ce131d5fc131f712
singularitti <singularitti@outlook.com>
2021-01-21 08:13:04 -0500

    Add back `bin` to `Mpiexec` & 2 more constructors

--- src/CLI.jl
+++ src/CLI.jl
@@ -11,11 +11,14 @@ Represent the `mpiexec` or `mpirun` executable. Must be combined with an actual 
 Type `?Mpiexec.host` to see the documentation of the `host` parameter, and so on.
 """
 struct Mpiexec <: Executable
+    bin
     "Specify the number of processes to use."
     np::UInt
     "Path to the `mpiexec` or `mpirun` executable."
     args::Vector{<:Pair}
 end
+Mpiexec(np::Integer, args::AbstractVector{<:Pair}; bin = "mpiexec") = Mpiexec(bin, np, args)
+Mpiexec(np::Integer) = Mpiexec("mpiexec", np, Pair[])
 
 function scriptify end
 
