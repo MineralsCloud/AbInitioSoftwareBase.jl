63519ae02ee826b930267695017fa968f3149404
Reno <25192197+singularitti@users.noreply.github.com>
2021-05-30 19:07:37 -0400

    Update docs of `Input`, `Namelist`, `Card`, `groupname`, & `asstring`

--- src/Inputs.jl
+++ src/Inputs.jl
@@ -2,7 +2,11 @@ module Inputs
 
 export groupname, writetxt, asstring
 
-"An abstract type representing an input object of ab initio software."
+"""
+    Input
+
+An abstract type representing an input object of ab initio software. All other input types should subtype `Input`.
+"""
 abstract type Input end
 
 abstract type InputEntry end  # Define this to make the `eltype` not `Any` if both `Namelist` & `Card` exist.
@@ -10,13 +14,32 @@ abstract type InputEntry end  # Define this to make the `eltype` not `Any` if bo
 """
     Namelist <: InputEntry
 
-The abstraction of an component of a `Input`, a basic Fortran data structure.
+Represent a component of an `Input`, a basic Fortran data structure.
 """
 abstract type Namelist <: InputEntry end
 
+"""
+    Card <: InputEntry
+
+Represent `CAR` or cards of an `Input` in VASP, Quantum ESPRESSO, etc.
+"""
 abstract type Card <: InputEntry end
+
+"""
+    groupname(::Namelist)
+
+Get the group name of a `Namelist`.
+"""
 function groupname end
 
+"""
+    asstring(object, config::FormatConfig)
+    asstring(object; kwargs...)
+
+Return a string representing a qualified `Input` or part of an `Input`, with formatting arguments `FormatConfig` or `kwargs`.
+
+See also: [`FormatConfig`](@ref)
+"""
 function asstring end
 
 """
