82ca3156cbb36faff9ecfe59283e28cea3d7b887
Reno <25192197+singularitti@users.noreply.github.com>
2021-08-28 07:01:43 -0400

    Fix docs of `MpiexecConfig`

--- src/Commands.jl
+++ src/Commands.jl
@@ -9,13 +9,12 @@ export MpiexecConfig, mpiexec
 abstract type CommandConfig end
 
 """
-    MpiexecConfig(; exe="mpiexec", np=0, options=Dict())
+    MpiexecConfig(; np=1, options=Dict())
 
 Represent an `mpiexec` executable.
 
 # Arguments
-- `exe::String="mpiexec"`: the path to the executable.
-- `np::UInt=0`: the number of processes used. If `np` is zero, it means no parallelization is performed.
+- `np::UInt=1`: the number of processes used.
 - `options::Dict{String,Any}=Dict()`: the options of `mpiexec`. See ["mpiexec(1) man page"](https://www.open-mpi.org/doc/v3.0/man1/mpiexec.1.php).
 """
 @option struct MpiexecConfig <: CommandConfig
