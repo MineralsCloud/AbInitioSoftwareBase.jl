bdf4a2f105c1e021fc40b50308bc3be112bc7775
Reno <25192197+singularitti@users.noreply.github.com>
2021-08-28 06:58:10 -0400

    Redefine `mpiexec` using `MPICH_jll.mpiexec` & `MpiexecConfig`

--- src/Commands.jl
+++ src/Commands.jl
@@ -1,6 +1,7 @@
 module Commands
 
-using Configurations: @option
+using Configurations: from_kwargs, @option
+import MPICH_jll
 
 export MpiexecConfig
 
@@ -21,5 +22,22 @@ Represent an `mpiexec` executable.
     np::UInt = 1
     options::Dict{String,Any} = Dict()
 end
+
+function mpiexec(config::MpiexecConfig)
+    args = [MPICH_jll.mpiexec_path, "-n", string(config.np)]
+    for (k, v) in config.options
+        push!(args, k, string(v))
+    end
+    return function (main::AbstractVector; env = String[], kwargs...)
+        append!(args, main)
+        cmd = Cmd(args)
+        if isempty(env)
+            return Cmd(cmd; env = MPICH_jll.mpiexec().env; kwargs...)
+        else
+            return Cmd(cmd; env = env, kwargs...)
+        end
+    end
+end
+mpiexec(; kwargs...) = mpiexec(from_kwargs(MpiexecConfig; kwargs...))
 
 end
